3ba8840e21f8332b470e0284419e79e52a76a7bd
Chris Sommers <cdsommers@gmail.com>
2025-04-24 04:23:47 -0400

    update models that can be used

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def main():
         type=str, 
         required=False, 
         default=None,
-        choices=["gpt-4", "gpt-4-0314", "gpt-4-32k", "gpt-4-32k-0314", "gpt-3.5-turbo", "gpt-3.5-turbo-0301", "text-davinci-003", "code-davinci-002", "text-davinci-001", "text-curie-001", "text-babbage-001", "text-ada-001"],
+        choices=["gpt-4", "gpt-4-0314", "gpt-4-32k", "gpt-4-32k-0314", "gpt-4-32k-0613", "gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-3.5-turbo-0301", "gpt-3.5-turbo-0613", "gpt-3.5-turbo-1106", "text-davinci-003", "code-davinci-002", "text-davinci-001", "text-curie-001", "text-babbage-001", "text-ada-001"],
         help="Model used. Default is set in the config file."
     )
 
